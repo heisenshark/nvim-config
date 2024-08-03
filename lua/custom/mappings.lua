c05d4173897e1dd130a26c7f17945c0986d23d13
heisenshark <sylaypaper@gmail.com>
2024-08-03 22:50:39 +0200

    deleted old mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -40,12 +40,6 @@ M.general = {
     -- moving multiple lines config
     ["<A-j>"] = { ":m '>+1<CR>gv" },
     ["<A-k>"] = { ":m '<-2<CR>gv" },
-    -- ["gM"] = {":lua print(vim.api.nvim_get_mode()['mode'])"},
-    ["gM"] = { function()
-      print(vim.api.nvim_get_mode()['mode'])
-      vim.api.nvim_input("10j")
-      -- return "10j"
-    end },
     [">"] = { ">gv", "indent" },
   },
 }
